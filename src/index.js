1c3aba853445bf5dce590919ad271f30a49f6640
kleyson castro <kleysoncastro99@gmail.com>
2022-01-24 21:54:12 -0300

    criaçao de conta e verificaçao: se ja existe cpf

--- src/index.js
+++ src/index.js
@@ -1,8 +1,32 @@
 const express = require('express');
+const { send, status, sendStatus } = require('express/lib/response');
+const {v4: uuidv4} = require("uuid");
+
+customers = []
 
 const app = express();
 app.use(express.json());
 
+app.post("/account", (req, res) => {
+    const {cpf, name } = req.body;
 
+    constAlreadyExist = customers.some(
+        (customer) => customer.cpf === cpf
+    );
+    if (constAlreadyExist) {
+        return res.status(400).json({
+        error: "CPF já cadastrado"
+        })
+    }
+   
+    customers.push({
+        cpf,
+        name,
+        id: uuidv4(),
+        statement: []
+    })
+    console.log(customers)
+    return res.status(201).send();
+})
 
 app.listen(3333);
