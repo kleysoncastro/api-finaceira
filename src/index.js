e46e7b506f62eaa63a3b91df2793b2bcb8202133
kleyson castro <kleysoncastro99@gmail.com>
2022-01-27 10:11:04 -0300

    middleware para verificar existecia de cpf cadastrado 	middleware faz a verificaçao do cpf 	se existir, injeta na request o customes do cpf encontrado

--- src/index.js
+++ src/index.js
@@ -7,6 +7,19 @@ customers = []
 const app = express();
 app.use(express.json());
 
+function verifyIfExisteAccountWhithCPF(req, res, next) {
+    const {cpf} = req.headers;
+
+    customer = customers.find(
+        (customer) =>  customer.cpf === cpf);
+        if(!customer) {
+            return res.status(400).send({error: "Customer not fund!"})
+        } 
+
+        req.customer = customer;
+      return  next();
+}
+
 app.post("/account", (req, res) => {
     const {cpf, name } = req.body;
 
@@ -29,16 +42,11 @@ app.post("/account", (req, res) => {
     return res.send();
 })
 
-app.get("/statement/:cpf", (req, res) => {
+app.get("/statement",verifyIfExisteAccountWhithCPF, (req, res) => {
 
-    const {cpf} = req.params;
-    const customer = customers.find(
-        (customer) => customer.cpf === cpf
-    );
-    if(customer === undefined) {
-        return res.status(400).send({error: "Customer not fund!"})
-    } 
-    return res.json(customer.statement)
+    const {customer} = req;
+
+    return res.send(customer.statement);
 });
 
 app.listen(3333);
